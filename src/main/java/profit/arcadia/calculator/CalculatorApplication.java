882a0807b8962de52d5fd5c9cec14732c5e4fe23
서용준 <syj0422@gachon.ac.kr>
2024-05-16 09:25:52 +0900

    first commit

---
+++ src/main/java/profit/arcadia/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package profit.arcadia.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/profit/arcadia/calculator/CalculatorTest.java
@@ -0,0 +1,27 @@
+package profit.arcadia.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SuppressWarnings("unused")
+public class CalculatorTest {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum(){
+        assertEquals(5, calculator.sum(2,3));
+    }
+    @Test
+    public void testSub(){
+        assertEquals(-3, calculator.sub(3,6));
+    }
+    @Test
+    public void testMul(){
+        assertEquals(10, calculator.mul(2,5));
+    }
+    @Test
+    public void testDiv(){
+        assertEquals(6, calculator.div(12,2));
+    }
+}
