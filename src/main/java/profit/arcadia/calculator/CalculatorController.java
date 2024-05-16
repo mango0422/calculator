67e74ec37b914179971f120de27c838f60232d07
서용준 <syj0422@gachon.ac.kr>
2024-05-16 10:31:26 +0900

    edit to getmapping

--- src/main/java/profit/arcadia/calculator/CalculatorController.java
+++ src/main/java/profit/arcadia/calculator/CalculatorController.java
@@ -1,8 +1,7 @@
 package profit.arcadia.calculator;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,7 @@ public class CalculatorController {
     @Autowired
     private Calculator calculator;
 
-    @RequestMapping("/sum")
+    @GetMapping("/sum")
     String sum(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
         return String.valueOf(calculator.sum(a, b));
     }
